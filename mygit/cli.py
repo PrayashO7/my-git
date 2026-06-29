d09d91da4ef409757ab15f98a3b31ecb8fafd189
Prayash <prayashdashcr7@gmail.com>
2026-06-29 14:42:00 +0530

    Refactor CLI commands for MyGit implementation

--- mygit/cli.py
+++ mygit/cli.py
@@ -1,23 +1,37 @@
+import argparse
 import sys
-from mygit.repository import init
-from mygit.objects import create_blob
+import os
+import repository
+import index
+
+def cmd_init(args):
+    repository.repo_init()
+
+def cmd_add(args):
+    if not os.path.exists('.mygit'):
+        print("fatal: not a mygit repository (or any of the parent directories)")
+        sys.exit(1)
+        
+    for path in args.path:
+        index.add_file(path)
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <command>")
-        return
+    parser = argparse.ArgumentParser(description="MyGit - A Git implementation from scratch")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+    
+    # Init Command
+    subparsers.add_parser('init', help='Initialize a new, empty repository')
+    
+    # Add Command
+    parser_add = subparsers.add_parser('add', help='Add file contents to the index')
+    parser_add.add_argument('path', nargs='+', help='Files to add')
+    
+    args = parser.parse_args()
+    
+    if args.command == 'init':
+        cmd_init(args)
+    elif args.command == 'add':
+        cmd_add(args)
 
-    command = sys.argv[1]
-
-    if command == "init":
-        init()
-
-    elif command == "add":
-        if len(sys.argv) < 3:
-            print("Usage: python main.py add <file>")
-            return
-
-        create_blob(sys.argv[2])
-
-    else:
-        print("Unknown command:", command)
+if __name__ == '__main__':
+    main()
